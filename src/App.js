7b30198fbef2a31c04c239cd22971be79d3c7050
Friedrich Chen <friedrichchen@Jalevers-MacBook-Pro.local>
2021-10-30 09:24:20 +0800

    update some files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from "react";
 import faker from "faker";
 import VList from "./VList";
 import { css } from "@emotion/css";
+import "./App.css";
+
+// const router = new VueRouter({ ... })
+
+// router.beforeEach((to, from, next) => {
+//   // ...
+// })
 
 const total = 100000;
 const data = [];
@@ -19,10 +26,7 @@ const bufferSize = 5;
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}; import React from "react";
-    import faker from "faker";
-    import VList from "./VList";
-    import { css } from "@emotion/css";
+    this.state = {};
 
     const total = 100000;
     const data = [];
@@ -32,78 +36,37 @@ class App extends React.Component {
         value: faker.lorem.sentences(),
       });
     }
-
-    const wrapperHeight = 667;
-    const estimateRowHeight = 66.7;
-    const bufferSize = 5;
-
-    class App extends React.Component {
-      constructor(props) {
-        super(props);
-        this.state = {};
-      }
-
-      render () {
-        return (
-          <VList
-            wrapperHeight={wrapperHeight}
-            estimateRowHeight={estimateRowHeight}
-            bufferSize={bufferSize}
-            total={total}
-            rowRender={(index, styleData) => {
-              return (
-                <div
-                  key={index}
-                  id={`item-${index}`}
-                  style={styleData}
-                  className={css`
-                    width: 100%;
-                    padding: 20px;
-                    border-bottom: 1px solid #000;
-                  `}
-                >
-                  <span style={{ width: '100%', display: 'flex', }}>{data[index].id}</span>
-                  <span style={{ width: '100%', display: 'flex', }}>{data[index].value}</span>
-                </div>
-              );
-            }}
-          >
-
-          </VList>
-        );
-      }
-    }
-
-    export default App;
   }
 
   render () {
     return (
-      <VList
-        wrapperHeight={wrapperHeight}
-        estimateRowHeight={estimateRowHeight}
-        bufferSize={bufferSize}
-        total={total}
-        rowRender={(index, styleData) => {
-          return (
-            <div
-              key={index}
-              id={`item-${index}`}
-              style={styleData}
-              className={css`
+      <React.Fragment>
+        <h1 class='global-title'>bracket Title</h1>
+        <VList
+          wrapperHeight={wrapperHeight}
+          estimateRowHeight={estimateRowHeight}
+          bufferSize={bufferSize}
+          total={total}
+          rowRender={(index, styleData) => {
+            return (
+              <div
+                key={index}
+                id={`item-${index}`}
+                style={styleData}
+                className={css`
                 width: 100%;
                 padding: 20px;
                 border-bottom: 1px solid #000;
               `}
-            >
-              <span style={{ width: '100%', display: 'flex', }}>{data[index].id}</span>
-              <span style={{ width: '100%', display: 'flex', }}>{data[index].value}</span>
-            </div>
-          );
-        }}
-      >
-
-      </VList>
+              >
+                <span style={{ width: '100%', display: 'flex', }}>{data[index].id}</span>
+                <span style={{ width: '100%', display: 'flex', }}>{data[index].value}</span>
+              </div>
+            );
+          }}
+        >
+        </VList>
+      </React.Fragment>
     );
   }
 }
